51fca2064122e5aba132776275925836ba4e86d1
Samuel Cristo da Fonseca <samfonsa12345@gmail.com>
2020-04-15 10:26:58 -0400

    Adding the Register and Login layout

--- app/src/main/java/com/example/organizze/MainActivity.java
+++ app/src/main/java/com/example/organizze/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.organizze;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.organizze.activity.LoginActivity;
+import com.example.organizze.activity.RegisterActivity;
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
-import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
 
 public class MainActivity extends IntroActivity {
 
@@ -40,8 +43,22 @@ public class MainActivity extends IntroActivity {
         addSlide(new FragmentSlide.Builder()
                 .background(android.R.color.white)
                 .fragment(R.layout.intro_fourth)
+                .build()
+        );
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_register)
                 .canGoForward(false)
                 .build()
         );
+    }
+
+    public void buttonLogin(View view){
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    public void clickRegister(View view){
+        startActivity(new Intent(this, RegisterActivity.class));
     }
 }
